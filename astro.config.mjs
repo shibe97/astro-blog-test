dffaf0fb26e871f96ad9069448965b7dc8640ae7
Kazuki Shibata <shibata@microcms.co.jp>
2025-02-07 15:41:37 +0900

    [update] draft modeを実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ export default defineConfig({
   adapter: vercel({
     isr: {
       expiration: 60,
+      bypassToken: 'f3a13dad-b3d3-635a-94b7-eed969401a99',
+      exclude: ['/draft'],
     },
   }),
 });
